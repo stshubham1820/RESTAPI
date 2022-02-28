1541d35a852903a7f79da14f2d4bf4815fcfe595
Shubham Tiwari <st18msd7@gmail.com>
2022-03-01 00:29:13 +0530

    finalcommit

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=100)
-    date = models.DateField(null=True)
+    date = models.DateField(auto_now_add=True)
     desc = models.TextField(null=True)
     status = models.BooleanField(null=True)
 
